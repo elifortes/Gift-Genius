b7a56b6fbb859b66183b3d970eb9401aee7e3f63
Eliane Fortes <elimrfortes@gmail.com>
2023-11-27 18:12:10 +1100

    update

---
+++ app/controllers/proposals_controller.rb
@@ -0,0 +1,35 @@
+class ProposalsController < ApplicationController
+
+  def index
+    @proposals = Proposal.all
+  end
+
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def show
+    @proposal = Proposal.find(params[:id])
+  end
+
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    # @proposal.user = current_user
+
+    if @proposal.save
+      redirect_to @proposal, notice: 'Proposal was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+    private
+
+  def proposal_params
+    params.require(:proposal).permit(:title, :description, :user_id)
+  end
+
+end
