dccf71c1eac9088b8c4446e64c036e29a0e7917b
Eliane Fortes <elimrfortes@gmail.com>
2023-11-30 15:39:18 +1100

    up

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -1,5 +1,6 @@
 class OccasionsController < ApplicationController
   def show
     @occasion = Occasion.find(params[:id])
+    @profile = @occasion.profile
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,9 @@
 class ProfilesController < ApplicationController
+
   def show
     @occasion = Occasion.find(params[:occasion_id])
+    @profile = @occasion.profile
+    @favorite = @profile.favorite
   end
 
   def edit
@@ -8,15 +11,22 @@ class ProfilesController < ApplicationController
     @profile = @occasion.profile
   end
 
+
+
   def update
     @profile = Profile.find(params[:id])
     @occasion = Occasion.find @profile.occasion_id
-    @favorite = Favorite.new(param_strong)
+    # @favorite = Favorite.new(param_strong)
+    @favorite = Favorite.new(favorites: params[:profile][:favorites], hobbies: params[:profile][:hobbies], activities: params[:profile][:activities])
     @favorite.occasion = @occasion
     @favorite.user = current_user
-    @favorite.save!
-    raise
+    if @favorite.save!
+      redirect_to @occasion, notice: 'Questionnaire is saved.'
+    else
+      render :new, alert: :unprocessable_entity
+    end
   end
+
 
   def questionnaire
     @profile = Profile.find(params[:id])
