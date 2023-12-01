83fb63c7fc62e9979f0d1d081f20371d83f2d6a8
menahil liaqat <zoyagee786@gmail.com>
2023-12-01 13:04:49 +1100

    page controller nill my contacts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,12 @@ class PagesController < ApplicationController
   def home
     return unless current_user
 
-    @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
+    # @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
+    if current_user.mycontact
+      @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
+    else
+      @mycontacts = []
+    end
     @mycontacts.sort_by! { |p| p.personnal.birthday }
     @myoccasions = current_user.myoccasions
   end
