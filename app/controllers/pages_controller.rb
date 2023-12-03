fa8a2a139e3017f26368cf6a90a3fcd5095fbb73
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-12-03 23:03:59 +1100

    pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,20 @@ class PagesController < ApplicationController
     # occasions
 
     return unless current_user
+    # looking for events i created
+    @myoccasions = current_user.myoccasions
+    @myoccasion = @myoccasions.first
+    # all information about this group
+    @groups = @myoccasion.groups.map { |user_id| User.includes(:personnal).find(user_id) }
+    @recipient = User.includes(:personnal).find(@myoccasion.recipient)
+    @gift = Gift.find(@myoccasion.gift)
+    # looking for product i choose for an existing event.
+    @occasions = current_user.occasions
+    @occasion = @occasion.first
+    @proposal = @occasion.proposal
+    @products = @proposal.products.where(position: 1)
+
+    # old code
     @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
     # @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
     if current_user.mycontact
@@ -16,6 +30,5 @@ class PagesController < ApplicationController
       @mycontacts = []
     end
     @mycontacts.sort_by! { |p| p.personnal.birthday }
-    @myoccasions = current_user.myoccasions
   end
 end
