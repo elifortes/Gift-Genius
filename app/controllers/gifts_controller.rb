50be71652c855b1915dcfb147488aa056884b93a
Eliane Fortes <elimrfortes@gmail.com>
2023-12-03 11:22:11 +1100

    updated occasion and gift price

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,13 +2,14 @@ class GiftsController < ApplicationController
   def create
     @gift = Gift.new(param_strong)
     @myoccasion = Myoccasion.find(params[:myoccasion_id])
-    #@myoccasion.gift = @gift.id
-    #@myoccasion.save!
-    @max_price = 1000
-    @min_price = 0
-    @gift.price = (params[:gift][:price].to_f / 100 * 1000).to_i
+    @myoccasion.gift = @gift.id
+    @myoccasion.save!
+    # @max_price = 1000
+    # @min_price = 0
+    # @gift.price = (params[:gift][:price].to_f / 100 * 1000).to_i
     if @gift.save!
       @myoccasion.gift = @gift.id
+
       @myoccasion.save!
       @myoccasion.groups.push(current_user.id).each do |o|
         user = User.find(o)
@@ -17,12 +18,15 @@ class GiftsController < ApplicationController
           gift: @gift.id,
         )
         occasion.myoccasion = @myoccasion
+
         occasion.user = user
         occasion.save!
         question = Question.new
         question.user = user
         question.occasion = occasion
         question.save!
+
+
       end
 
       redirect_to myoccasion_confirmation_path(@myoccasion)
--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -1,6 +1,15 @@
 class OccasionsController < ApplicationController
   def show
     @occasion = Occasion.find(params[:id])
+    @gift = Gift.find(@occasion.gift)
+
+    # @myoccasion = Myoccasion.find(params[:id])
     @question = @occasion.question
+
+
+
+
   end
+
+
 end
