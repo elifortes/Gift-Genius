b64df5adebe5aa2a2f66f02354e28ad48ad891e3
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-12-05 16:25:45 +1100

    gift occasion

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,11 @@ class PagesController < ApplicationController
     return unless current_user
     # looking for events i created
     @myoccasions = current_user.myoccasions
-
-
     # looking for product i choose for an existing event.
-    occasions = current_user.occasions
+    @occasions = current_user.occasions
+    @ids_to_reject = @myoccasions.map { |myoccasion| myoccasion.id }
+    @occasions = @occasions.map { |occasion| occasion unless @ids_to_reject.member?(occasion.myoccasion_id) }
+    @occasions.compact!
 
     # old code
     @mycontacts = current_user.mycontact.contacts.map { |c| User.find(c) }
