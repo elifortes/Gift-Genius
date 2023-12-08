354492f8a2594b1a0e9e1dffc9bdbb5a8ccfc611
nakharinh francois-xavier phomsouvanh <elektrodynamiks@me.com>
2023-12-08 14:06:20 +1100

    openai

--- db/migrate/20231207221530_add.rb
+++ db/migrate/20231207221530_add.rb
@@ -1,4 +1,13 @@
 class Add < ActiveRecord::Migration[7.1]
   def change
+    add_column :myoccasions, :selected, :boolean, default: false
+    add_column :proposals, :selected, :boolean, default: false
+    add_column :occasions, :selected, :boolean, default: false
+    add_column :myoccasions, :selection, :jsonb
+    add_column :proposals, :selection, :jsonb
+    add_column :occasions, :selection, :jsonb
+    add_column :users, :name, :string
+    add_column :users, :birthday, :date
+    add_column :users, :sex, :string
   end
 end
